b1a049336b2491221a651b09dd23d56fab8a0e2d
¨FerPascualF50¨ <pascualefernando@gmail.com>
2023-06-19 18:53:15 -0300

    feat(add: borrar usuarios)

--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/interfaces/usuario';
@@ -12,22 +13,22 @@ import { UsuarioService } from 'src/app/services/usuario.service';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
- 
+
   listUsuarios: Usuario[] = [];
-  
+
   displayedColumns: string[] = ['usuario', 'nombre', 'apellido', 'sexo', 'acciones'];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  
-  constructor(private _usuarioServce: UsuarioService){}
+
+  constructor(private _usuarioServce: UsuarioService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.cargarUsuarios();
   }
 
-  cargarUsuarios(){
+  cargarUsuarios() {
     this.listUsuarios = this._usuarioServce.getUsuario();
     this.dataSource = new MatTableDataSource(this.listUsuarios)
   }
@@ -40,5 +41,17 @@ export class UsuariosComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
+  eliminarUsuario(index: number) {
+    console.log(index)
+    this._usuarioServce.eliminarUsuario(index);
+    this.cargarUsuarios();
+
+    this._snackBar.open('Usuario eliminado con exito', '', {
+      duration: 1500,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    })
   }
 }
