50eb54ee36ffeac3cb1c56f0cc67988b7691f262
¨FerPascualF50¨ <pascualefernando@gmail.com>
2023-06-19 12:35:01 -0300

    feat(control form  login)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 //Componentes
 import { LoginComponent } from './components/login/login.component';
 
 //Material
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 
 @NgModule({
@@ -25,7 +28,9 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  form: FormGroup
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar){
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+  ngOnInit(): void{
+  }
+
+  ingresar(){
+    const usuario = this.form.value.usuario;
+    const password = this.form.value.password;
+
+    if( usuario === 'fpascual' && password === 'admin123') {
+      // redirect to dashboard
+    } else {
+      this.error();
+    }
+  }
+
+  error() {
+    this._snackBar.open('Usuario o Contraseña invalidos', '', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    })
+  }
 }
